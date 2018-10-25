fa729202a02371f11373268ae6c5e26d912c1b63
Sergi-13 <sergi.cendrassos@gmail.com>
2018-10-25 11:32:29 +0200

    Programa amb O y README

--- Programa_vocals.java
+++ Programa_vocals.java
@@ -8,15 +8,26 @@ public class Main {
 	Scanner teclat = new Scanner(System.in);
         System.out.println("Introdueix la frase");
         String frase = teclat.nextLine();
-        int contador=0;
+        int contadorA=0;
+        int contadorE=0;
+        int contadorI=0;
+        int contadorO=0;
+        int contadorU=0;
         String[] caracters = frase.split("");
         for (int i = 0; i <caracters.length; i++) {
             String caracter = caracters[i];
-            if (caracter.equalsIgnoreCase("A") || caracter.equalsIgnoreCase("E") || caracter.equalsIgnoreCase("I")
-		|| caracter.equalsIgnoreCase("O") || caracter.equalsIgnoreCase("U")) {
-                contador++;
+            if (caracter.equalsIgnoreCase("A")) {
+                contadorA++;
+            } else if (caracter.equalsIgnoreCase("E")) {
+                contadorE++;
+            } else if (caracter.equalsIgnoreCase("I")) {
+                contadorI++;
+            } else if (caracter.equalsIgnoreCase("O")) {
+                contadorO++;
+            } else if (caracter.equalsIgnoreCase("U")) {
+                contadorU++;
             }
         }
-        System.out.println("A la frase hi ha "+contador+" vocals.");
+        System.out.println("A la frase hi ha "+contadorA+" A, "+contadorE+" E, "+contadorI+" I, "+contadorO+" O, i "+contadorU+" U.");
     }
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+Aquest programa demana introduir una frase.
+Després identifica quantes vocals hi ha a al frase introduïda i després indica quines d'aquelles vocals són O.
